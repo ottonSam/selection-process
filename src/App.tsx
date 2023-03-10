f23593442339ff7946f27c5e908ed408ce5389c7
ottonsam <ottonsam@gmail.com>
2023-03-09 23:37:43 -0300

    set dark theme on default

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import usePersistedState from "./utils/usePersistedState";
+
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+
+import GlobalStyle from "./styles/global";
+
 import { FormPage } from "./pages/FormPage";
 import { LandingPage } from "./pages/LandingPage";
 
 function App() {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>("theme", dark);
   const [showForm, setShowForm] = useState(false);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
 
   const handleShowForm = () => {
     setShowForm(!showForm);
   };
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       {!showForm ? (
         <LandingPage goToForm={handleShowForm} />
       ) : (
         <FormPage goToHome={handleShowForm} />
       )}
-    </div>
+    </ThemeProvider>
   );
 }
 
