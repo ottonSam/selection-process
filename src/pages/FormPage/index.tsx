3a99928fbe3e12a776e96d857b163e29256393c2
ottonsam <ottonsam@gmail.com>
2023-03-13 01:12:26 -0300

    form validation

--- src/pages/FormPage/index.tsx
+++ src/pages/FormPage/index.tsx
@@ -1,27 +1,63 @@
+import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { FormContainer, FormLabel, SubmitButton } from "./styles";
+
 import AuthRadio from "../../components/AuthRadio";
 import FileField from "../../components/FileField";
 import GitData from "../../components/GitData";
 import TecCheckBox from "../../components/TecCheckBox";
 import TextArea from "../../components/TextArea";
 import TextField from "../../components/TextField";
-import { FormContainer, FormLabel, SubmitButton } from "./styles";
+import validationSchema from "../../utils/validationSchema";
+
 interface IProps {
   goToHome: () => void;
 }
 
+interface IFormInputs {
+  git_user: string;
+  name: string;
+  image: string;
+  repository: string;
+  email: string;
+  description: string;
+  react: boolean;
+  node: boolean;
+  docker: boolean;
+  git: boolean;
+  insomnia: boolean;
+  curriculum: File;
+  auth: boolean;
+}
+
 const FormPage = ({ goToHome }: IProps) => {
+  const methods = useForm<IFormInputs>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const formSubmitHandler: SubmitHandler<IFormInputs> = (data: IFormInputs) => {
+    alert(JSON.stringify(data));
+  };
   return (
     <FormContainer>
-      <FormLabel>Formulário de submissão</FormLabel>
-      <GitData />
-      <TextField name="email" label="Email" />
-      <TextArea />
-      <TecCheckBox />
-      <FileField />
-      <AuthRadio />
-      <SubmitButton onClick={goToHome} variant="outlined" color="secondary">
-        Enviar currículo
-      </SubmitButton>
+      <FormProvider {...methods}>
+        <form
+          autoComplete="off"
+          onSubmit={methods.handleSubmit(formSubmitHandler)}
+        >
+          <FormLabel>Formulário de submissão</FormLabel>
+          <GitData />
+          <TextField name="email" label="Email" />
+          <TextArea />
+          <TecCheckBox />
+          <FileField />
+          <AuthRadio />
+          <SubmitButton type="submit" variant="outlined" color="secondary">
+            Enviar currículo
+          </SubmitButton>
+        </form>
+      </FormProvider>
     </FormContainer>
   );
 };
---
+++ src/utils/validationSchema.ts
@@ -0,0 +1,19 @@
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  git_user: yup.string().min(3).required(),
+  name: yup.string().required(),
+  image: yup.string(),
+  repository: yup.string().required(),
+  email: yup.string().required(),
+  description: yup.string().min(50).max(1000).required(),
+  react: yup.boolean().required().default(false),
+  node: yup.boolean().required().default(false),
+  docker: yup.boolean().required().default(false),
+  git: yup.boolean().required().default(false),
+  insomnia: yup.boolean().required().default(false),
+  curriculum: yup.mixed().required(),
+  auth: yup.boolean().required(),
+});
+
+export default validationSchema;
