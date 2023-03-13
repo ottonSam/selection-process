dc35094cc7f4ba215b2a4440f29407502d508f69
ottonsam <ottonsam@gmail.com>
2023-03-13 14:29:35 -0300

    validation message

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,22 +1,38 @@
 import * as yup from "yup";
 
 const validationSchema = yup.object().shape({
-  git_user: yup.string().min(3).required(),
-  name: yup.string().required(),
-  image: yup.string(),
+  git_user: yup.string().required("É necessário informar os dados do Git."),
+  name: yup
+    .string()
+    .required("Houve algum erro em recuperar os dados da api do Git."),
+  image: yup.string().default("Imagem não informada."),
   repository: yup.object({
-    label: yup.string().required(),
-    value: yup.string().required(),
+    label: yup
+      .string()
+      .required("Houve algum erro em recuperar os dados da api do Git."),
+    value: yup
+      .string()
+      .required("Houve algum erro em recuperar os dados da api do Git."),
   }),
-  email: yup.string().required(),
-  description: yup.string().min(50).max(1000).required(),
+  email: yup.string().required("O email é necessário."),
+  description: yup
+    .string()
+    .max(1000, "A descrição está muito longa.")
+    .required("A descrição é obrigatório."),
   react: yup.boolean().required().default(false),
   node: yup.boolean().required().default(false),
   docker: yup.boolean().required().default(false),
   git: yup.boolean().required().default(false),
   insomnia: yup.boolean().required().default(false),
-  curriculum: yup.mixed().required(),
-  auth: yup.boolean().required(),
+  curriculum: yup.mixed().required("É necessário informar seu currículo."),
+  auth: yup
+    .boolean()
+    .required()
+    .default(true)
+    .oneOf(
+      [true],
+      "É preciso aceitar o uso dos dados para o envio do formulário."
+    ),
 });
 
 export default validationSchema;
