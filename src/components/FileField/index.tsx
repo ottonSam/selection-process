a1a34cf82465472fa4713d91eb548fe58df8b19c
ottonsam <ottonsam@gmail.com>
2023-03-13 01:46:18 -0300

    fileField validate

--- src/components/FileField/index.tsx
+++ src/components/FileField/index.tsx
@@ -1,7 +1,33 @@
-import { FileFieldComponent } from "./styles";
+import { FormLabel } from "@mui/material";
+import { Controller, useFormContext } from "react-hook-form";
+import { Container, FileFieldComponent } from "./styles";
 
 const FileField = () => {
-  return <FileFieldComponent type="file" />;
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <Container>
+      <FormLabel>Currículo</FormLabel>
+      <Controller
+        name="curriculum"
+        control={control}
+        defaultValue=""
+        render={({ field }) => {
+          return (
+            <FileFieldComponent
+              {...field}
+              type="file"
+              variant="outlined"
+              error={!!errors.curriculum}
+              helperText={errors.curriculum?.message?.toString()}
+            />
+          );
+        }}
+      />
+    </Container>
+  );
 };
 
 export default FileField;
--- src/components/FileField/styles.tsx
+++ src/components/FileField/styles.tsx
@@ -1,6 +1,12 @@
+import styled from "styled-components";
 import { styled as MuiStyled, TextField } from "@mui/material";
 
-export const FileFieldComponent = MuiStyled(TextField)`
+export const Container = styled.div`
   margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+export const FileFieldComponent = MuiStyled(TextField)`
+  margin-top: 0.5rem;
   width: 100%
 `;
